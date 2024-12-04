b02971e48a3e9cca74020b9f7365bddd8126b466
archenovalis <archelaus.novalis@gmail.com>
2024-12-04 03:26:56 -0700

    another xsddiff update

--- pythonScripts/xsddiff.py
+++ pythonScripts/xsddiff.py
@@ -20,16 +20,17 @@ def process_files():
     elementType_names = []
     with open(filename, 'r', encoding='utf-8') as file:
       content = file.read()
-      groupMatches = re.findall(r'<xs:group name="(.*?)">', content)
-      group_names.extend(groupMatches)
+# correct code for md.xsd, seems right now
+#      groupMatches = re.findall(r'<xs:group name="(.*?)"\s*>', content)
+#      group_names.extend(groupMatches)
+#      complexMatches = re.findall(r'<xs:complexType name="(.*?)"\s*>', content)
+#      complex_names.extend(complexMatches)
+#      if (group_names == [] and complex_names == []):
       elementTypeMatches = re.findall(r'<xs:element name="(.*?)".*type="(.*?)"', content)
       elementType_names.extend(elementTypeMatches)
-      if (elementType_names == []):
-        complexMatches = re.findall(r'<xs:complexType name="(.*?)">', content)
-        complex_names.extend(complexMatches)
-      if (complex_names == []):
-        elementMatches = re.findall(r'(?!.*type).*?<xs:element name="(.*?)"', content)
-        element_names.extend(elementMatches)
+#      if (elementType_names == []):
+      elementMatches = re.findall(r'(?!.*type).*?<xs:element name="(.*?)"', content)
+      element_names.extend(elementMatches)
   
     # Process diff.xsd
     newdiff = diff
@@ -44,7 +45,8 @@ def process_files():
     match = re.search(r'\n(\s*).*lax.*',newdiff)
     if match:
       spacing = match[1]
-      choice_block = spacing+'<xs:choice>\n'
+      names = []
+      choice_block = spacing+'<xs:choice minOccurs="1" maxOccurs="unbounded">\n'
       if (group_names != []):
         choice_block = spacing+'<xs:choice>\n'
         for group in group_names:
@@ -52,12 +54,16 @@ def process_files():
       if (complex_names != []):
         for complex in complex_names:
           choice_block = choice_block+spacing+f'  <xs:element name="{complex}" />\n'
-      if (element_names != []):
-        for complex in element_names:
-          choice_block = choice_block+spacing+f'  <xs:element name="{complex}" />\n'
       if (elementType_names != []):
         for complex in elementType_names:
-          choice_block = choice_block+spacing+f'  <xs:element name="{complex[0]}" type="{complex[1]}" />\n'
+          if complex[0] not in names:
+            choice_block = choice_block+spacing+f'  <xs:element name="{complex[0]}" type="{complex[1]}" />\n'
+            names.append(complex[0])
+      if (element_names != []):
+        for complex in element_names:
+          if complex not in names:
+            choice_block = choice_block+spacing+f'  <xs:element name="{complex}" />\n'
+            names.append(complex)
 
       choice_block = choice_block+spacing+'</xs:choice>'
       newdiff = re.sub(
